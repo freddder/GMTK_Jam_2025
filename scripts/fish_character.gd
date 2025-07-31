42ed72c484f8044b453933764ef02acd1b5fc7a2
bionicle564 <57322673+bionicle564@users.noreply.github.com>
2025-07-31 16:23:43 -0400

    dictionary for fish parts added, stored with stats

--- scripts/fish_character.gd
+++ scripts/fish_character.gd
@@ -5,6 +5,8 @@ class Attributes:
 	var health: int = 0
 	var attack: int = 0
 	var speed: int = 0
-
+	var body: String = "debug_body"
+	var tail: String = "debug_tail"
+	var deco: String = "debug_deco"
 
 var attributes: Attributes

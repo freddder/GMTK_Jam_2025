c93bc308e6959097e0224c7cf0e708abaa43dbf2
bionicle564 <57322673+bionicle564@users.noreply.github.com>
2025-08-01 13:27:04 -0400

    path being drawn using temporary fish

--- scripts/timeline_manager.gd
+++ scripts/timeline_manager.gd
@@ -8,9 +8,52 @@ var subPath: Array[cell] = []
 
 @export var totalLength:int = 20
 
+@onready var playerSprite = load("res://assets/sprites/Icons/fish_icon.png")
+@onready var mateSprite = load("res://assets/sprites/Icons/heart_icon.png")
+@onready var eventSprite = load("res://assets/sprites/Icons/questionmark_icon.png")
+@onready var fightSprite = load("res://assets/sprites/Icons/fight_icon.png")
+
+class playerIcon:
+	var icon:Sprite2D
+	var pos:Vector2
+
 
 class cell:
 	var type:ePoi_type
+
+
+func generateVisuals():
+	var scale = .35
+	var col = Color(0,0,.2)
+	for i in totalLength:
+		var track:Sprite2D = Sprite2D.new()
+		track.texture = playerSprite
+		track.self_modulate = col
+		track.position.x = i * 128 * scale + 64
+		track.position.y = 128
+		track.scale = Vector2(scale,scale)
+		self.add_child(track)
+		
+		if mainPath[i].type == ePoi_type.PATH_UP:
+			for j in topPath.size():
+				var uptrack:Sprite2D = Sprite2D.new()
+				uptrack.texture = playerSprite
+				uptrack.self_modulate = col
+				uptrack.position.x = (j * 128 * scale) + (i * 128 * scale) + 64
+				uptrack.position.y = 96
+				uptrack.scale = Vector2(scale,scale)
+				self.add_child(uptrack)
+		if mainPath[i].type == ePoi_type.PATH_DOWN:
+			for j in subPath.size():
+				var subtrack:Sprite2D = Sprite2D.new()
+				subtrack.texture = playerSprite
+				subtrack.self_modulate = col
+				subtrack.position.x = (j * 128 * scale) + (i * 128 * scale) + 64
+				subtrack.position.y = 164
+				subtrack.scale = Vector2(scale,scale)
+				self.add_child(subtrack)
+		
+	pass
 
 func generateTimeline():
 	var hasTop = false
@@ -27,7 +70,8 @@ func generateTimeline():
 	# go through the cells and make them follow the rules
 	for i in totalLength - 1:
 		if mainPath[i].type == ePoi_type.PATH_UP && !hasTop: # generate the top path at the first oppurtunity
-			var length = randi() % (totalLength - i)
+			var length = randi() % (totalLength - 1 - i)
+			if length < 3: length = 3
 			mainPath[i + length].type = ePoi_type.NOTHING
 			for j in length - 1:
 				var newCell = cell.new()
@@ -41,7 +85,8 @@ func generateTimeline():
 			mainPath[i].type = ePoi_type.NOTHING
 		
 		if mainPath[i].type == ePoi_type.PATH_DOWN && !hasSub:# generate the sub path at the first oppurtunity
-			var length = randi() % (totalLength - i)
+			var length = randi() % (totalLength - 1 - i)
+			if length < 3: length = 3
 			mainPath[i + length].type = ePoi_type.NOTHING
 			for j in length - 1:
 				var newCell = cell.new()
@@ -53,7 +98,7 @@ func generateTimeline():
 			hasSub = true
 		elif mainPath[i].type == ePoi_type.PATH_DOWN:
 			mainPath[i].type = ePoi_type.NOTHING
-		
+	
 	
 	
 	var finalCell = cell.new()
@@ -64,4 +109,5 @@ func generateTimeline():
 # for debugging
 func _ready() -> void:
 	generateTimeline()
+	generateVisuals()
 	var sadf = 0

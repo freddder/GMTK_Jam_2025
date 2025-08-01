bc2cdde15e7a518ccf5ef29bdedaaf68fec12b03
Lucas Magalhaes <lucasfredmagalhaes@gmail.com>
2025-08-01 19:26:17 -0400

    WIP text box manager

--- globals/text_box_manager.gd
+++ globals/text_box_manager.gd
@@ -1,1 +1,17 @@
 extends Node
+
+func _ready():
+	# Initialize TextBox scene on the tree
+	var scene = load("res://globals/TextBox.tscn")
+	var instance = scene.instantiate()
+	get_tree().root.add_child.call_deferred(instance)
+
+func update_text(text: String):
+	var text_box : MarginContainer = get_node("/root/TextBox/MarginContainer")
+	var text_label : RichTextLabel = text_box.get_node("RichTextLabel")
+	text_label.clear()
+	text_label.add_text(text)
+
+	text_box.position.y += text_box.size.y
+	var tween := create_tween()
+	tween.tween_property(text_box, "position:y", -text_box.size.y, 1).as_relative().set_trans(Tween.TRANS_QUART)

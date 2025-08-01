a0ab9d333771074014c51d21797c9bf8040bb879
Lucas Magalhaes <lucasfredmagalhaes@gmail.com>
2025-08-01 12:58:46 -0400

    Finished battle functionality

--- globals/battle_manager.gd
+++ globals/battle_manager.gd
@@ -6,20 +6,27 @@ var enemy_fish: FishCharacter
 var action_bar_max_amount: float = 100.0
 var is_battle_active: bool = false
 
+func battle_start():
+	player_fish.curr_health = player_fish.profile.attributes.data[Attributes.Type.Health]
+	enemy_fish.curr_health = enemy_fish.profile.attributes.data[Attributes.Type.Health]
+	is_battle_active = true
+
 func _process(delta):
 	if not is_battle_active:
 		return
 
-	player_fish.curr_action_amount += player_fish.attributes.data[Attributes.Type.Speed] * delta
-	enemy_fish.curr_action_amount += enemy_fish.attributes.data[Attributes.Type.Speed] * delta
+	player_fish.curr_action_amount += player_fish.profile.attributes.data[Attributes.Type.Speed] * delta
+	enemy_fish.curr_action_amount += enemy_fish.profile.attributes.data[Attributes.Type.Speed] * delta
 
 	if player_fish.curr_action_amount >= action_bar_max_amount:
-		var total_attack_power = player_fish.attributes.data[Attributes.Type.Attack]
+		print("player attack")
+		var total_attack_power = player_fish.profile.attributes.data[Attributes.Type.Attack]
 		enemy_fish.get_attacked(total_attack_power)
 		player_fish.curr_action_amount -= action_bar_max_amount
 
 	if enemy_fish.curr_action_amount >= action_bar_max_amount:
-		var total_attack_power = enemy_fish.attributes.data[Attributes.Type.Attack]
+		print("enemy attack")
+		var total_attack_power = enemy_fish.profile.attributes.data[Attributes.Type.Attack]
 		player_fish.get_attacked(total_attack_power)
 		enemy_fish.curr_action_amount -= action_bar_max_amount
 
@@ -27,5 +34,5 @@ func _process(delta):
 		print("Enemy won")
 		is_battle_active = false
 	elif enemy_fish.curr_health <= 0:
-		print("Plaeyer won")
+		print("Player won")
 		is_battle_active = false
--- scripts/fish_character.gd
+++ scripts/fish_character.gd
@@ -1,14 +1,13 @@
 class_name FishCharacter
 extends Node2D
 
-var attributes: Attributes
 var profile: FishProfile
 
 var body: String = "debug_body"
 var tail: String = "debug_tail"
 var deco: String = "debug_deco"
 
-var curr_health: int
+var curr_health: int = 0
 var curr_action_amount: float = 0.0
 signal took_damage(health_lost: int)
 
@@ -16,15 +15,12 @@ signal took_damage(health_lost: int)
 @onready var action_bar : ProgressBar = $Control/VBoxContainer/ActionBar
 
 func _ready() -> void:
-	attributes = Attributes.create()
-	curr_health = attributes.Type.Health
-
 	took_damage.connect(update_health_bar)
 
 func _process(delta):
 	# TODO: not run battle manager logic when it's not a battle
 	if action_bar != null:
-		action_bar.value = curr_action_amount / BattleManager.action_bar_max_amount
+		action_bar.value = float(curr_action_amount) / float(BattleManager.action_bar_max_amount) * 100.0
 
 func set_profile(in_profile: FishProfile) -> void:
 	profile = in_profile
@@ -37,5 +33,5 @@ func get_attacked(incoming_damage: int) -> int:
 	took_damage.emit(incoming_damage)
 	return incoming_damage
 
-func update_health_bar():
-	health_bar.value = curr_health / attributes.Type.Health
+func update_health_bar(asm: int):
+	health_bar.value = float(curr_health) / float(profile.attributes.data[Attributes.Type.Health]) * 100.0

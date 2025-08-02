8b7e120e0d8a9f0623186272b1f9c78dd4cd798f
Lucas Magalhaes <lucasfredmagalhaes@gmail.com>
2025-08-02 17:15:24 -0400

    Added stats window

--- scripts/fish_player_character.gd
+++ scripts/fish_player_character.gd
@@ -1,9 +1,12 @@
 class_name FishPlayerCharacter
 extends FishCharacter
 
+signal profile_update
+
 func _ready() -> void:
 	set_profile(FishProfile.create())
 	super._ready()
+
 
 
 func set_profile(in_profile: FishProfile) -> void:
@@ -11,3 +14,4 @@ func set_profile(in_profile: FishProfile) -> void:
 		EventBus.on_player_reincarnated.emit(profile)
 
 	super.set_profile(in_profile)
+	profile_update.emit()
--- scripts/player_inventory.gd
+++ scripts/player_inventory.gd
@@ -3,9 +3,14 @@ extends Control
 @onready var player: FishPlayerCharacter = get_tree().get_first_node_in_group("player")
 @onready var slot_scene := preload("res://scenes/player_inventory_slot.tscn")
 
+@onready var health_label : Label = $StatsMenu/MarginContainer/VBoxContainer/HBoxContainer/HealthLabel
+@onready var attack_label : Label = $StatsMenu/MarginContainer/VBoxContainer/HBoxContainer2/AttackLabel
+@onready var speed_label : Label = $StatsMenu/MarginContainer/VBoxContainer/HBoxContainer3/SpeedLabel
+
 func _ready() -> void:
 	player.on_inventory_changed.connect(_on_inventory_changed)
 	_on_inventory_changed()
+	player.profile_update.connect(update_stats_menu)
 
 
 func _on_inventory_changed() -> void:
@@ -13,3 +18,8 @@ func _on_inventory_changed() -> void:
 		var slot: PlayerInventorySlot = slot_scene.instantiate()
 		$HBoxContainer.add_child(slot)
 		slot.set_icon(player.inventory[i])
+
+func update_stats_menu():
+	health_label.text = str(player.profile.attributes.data[Attributes.Type.Health])
+	attack_label.text = str(player.profile.attributes.data[Attributes.Type.Attack])
+	speed_label.text = str(player.profile.attributes.data[Attributes.Type.Speed])

6694ee44ce24149fce9605210880cdd2dbe63bbb
Tonetfal <antonio.sid99@gmail.com>
2025-08-02 21:12:47 +0200

    item effects

--- scripts/fish_character.gd
+++ scripts/fish_character.gd
@@ -3,15 +3,17 @@ extends Node2D
 
 signal on_inventory_changed
 signal on_took_damage(health_lost: int)
+signal on_healed(health_received: int)
+signal on_life_stolen(life_stolen: int)
+signal on_crit_rolled
 
 @onready var health_bar : ProgressBar = %HealthBar
 @onready var action_bar : ProgressBar = %ActionBar
 
+# The inventory has to limit
+@export var inventory: Array[Item]
 
 var profile: FishProfile
-
-# The inventory has to limit
-var inventory: Array[Item]
 
 var body: String = "debug_body"
 var tail: String = "debug_tail"
@@ -19,6 +21,7 @@ var deco: String = "debug_deco"
 
 var curr_health: int = 0
 var curr_action_amount: float = 0.0
+var sent_hits: int = 0
 
 
 func _ready() -> void:
@@ -41,9 +44,7 @@ func deal_damage(damage: int) -> int:
 
 	# Decrease scaled damage by 30%; the effect stacks up, but is weaker
 	# 10.0 -> 7.0 -> 4.9 -> 3.43 ...
-	for item in inventory:
-		if item.type == Item.Type.BUBBLE_SHIELD:
-			scaled_damage *= 0.7
+	scaled_damage *= pow(0.7, get_inventory_item_count(Item.Type.BUBBLE_SHIELD))
 
 	# Round up: make sure to deal at least 1 damage
 	var final_damage: int = max(1, ceil(scaled_damage))
@@ -57,12 +58,27 @@ func deal_damage(damage: int) -> int:
 	return final_damage
 
 
+func heal(value: int) -> void:
+	var new_health: int = min(get_max_health(), curr_health + value)
+	if curr_health != new_health:
+		var delta := new_health + curr_health
+		curr_health = new_health
+
+		on_healed.emit(delta)
+
+
 func _on_damage_taken(health_lost: int) -> void:
 	update_health_bar()
 
 
 func get_max_health() -> int:
 	return max(profile.attributes.data[Attributes.Type.Health], 1) * 5
+
+
+func get_normalized_health() -> float:
+	var max := get_max_health()
+	var ratio := float(curr_health) / float(max)
+	return ratio
 
 
 func update_health_bar():
@@ -90,3 +106,47 @@ func shake(amplitude: float, duration: float, count: int) -> void:
 
 	tween.play()
 	await tween.finished
+
+
+func prepare_to_battle() -> void:
+	curr_health = get_max_health()
+	update_health_bar()
+	sent_hits = 0
+
+
+func get_inventory_item_count(type: Item.Type) -> int:
+	var count: int = 0
+	for item in inventory:
+		if item.type == type:
+			count += 1
+
+	return count
+
+
+func get_damage() -> int:
+	var calculator_count := get_inventory_item_count(Item.Type.BROKEN_CALCULATOR)
+
+	var damage = profile.attributes.data[Attributes.Type.Attack]
+	damage += calculator_count * sent_hits
+
+	if get_normalized_health() <= 0.25:
+		var poster_count := get_inventory_item_count(Item.Type.MOTIVATIONAL_POSTER)
+		damage *= pow(1.25, poster_count)
+
+	var lens_count := get_inventory_item_count(Item.Type.CONTACT_LENSES)
+	for i in lens_count:
+		if randf() > 0.75:
+			damage *= 2
+			on_crit_rolled.emit()
+
+	return damage
+
+
+func on_attack_sent(damage: int) -> void:
+	sent_hits += 1
+
+	var teeth_count := get_inventory_item_count(Item.Type.FAKE_VAMPIRE_TEETH)
+	if teeth_count > 0:
+		var value := damage * pow(0.4, teeth_count)
+		heal(value)
+		on_life_stolen.emit(value)

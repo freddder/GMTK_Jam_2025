747110b15060817836a2a3e9acc9d37dd830b1cb
Tonetfal <antonio.sid99@gmail.com>
2025-08-02 15:38:56 +0200

    disallow progress while action is ongoing

--- scripts/main_level.gd
+++ scripts/main_level.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
-@onready var battle_scene = load("res://scenes/battle.tscn")
+@onready var battle_scene := load("res://scenes/battle.tscn")
 @onready var player := $PlayerCharacter
 @onready var map_scene := $TimelineManager
 
 var active_battle: Battle
+var can_handle_action_input: bool = false
 
 
 func _ready() -> void:
@@ -13,12 +14,14 @@ func _ready() -> void:
 	map_scene.on_tile_landed.connect(_on_tile_landed)
 
 	# Start the game by mating; this will need to be moved after initial cut-scene
-	#await _promote_mating()
+	await _promote_mating()
 	map_scene.initialize()
 
+
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouse and event.is_pressed():
-		map_scene.on_action_completed(false)
+	if can_handle_action_input:
+		if event.is_action_pressed("lmb"):
+			map_scene.on_action_completed(false)
 
 
 func _on_tile_landed(type: TimelineManager.Type) -> void:
@@ -26,19 +29,20 @@ func _on_tile_landed(type: TimelineManager.Type) -> void:
 		TimelineManager.Type.FIGHT:
 			await _start_battle()
 			await _promote_random_rewards()
-			
 
 		TimelineManager.Type.MATE:
 			await _promote_mating()
-		
+
 		TimelineManager.Type.EVENT:
 			print("weed")
-		
+
 		TimelineManager.Type.BOSS:
 			print("boss fight")
 
 
 func _promote_mating() -> void:
+	can_handle_action_input = false
+
 	var initial_fish_profiles := FishRandomizer.randomize_many(3, player.profile.level,
 	player.profile.attributes, player.inventory)
 	var rewards := FishProfile.to_reward_array(initial_fish_profiles)
@@ -46,14 +50,22 @@ func _promote_mating() -> void:
 	$RewardSelectionScreen.show_rewards(rewards)
 	await $RewardSelectionScreen.on_option_selected
 
+	can_handle_action_input = true
+
 
 func _promote_random_rewards() -> void:
+	can_handle_action_input = false
+
 	var rewards := Item.to_reward_array(Item.get_random_count(3))
 	$RewardSelectionScreen.show_rewards(rewards)
 	await $RewardSelectionScreen.on_option_selected
 
+	can_handle_action_input = true
+
 
 func _start_battle() -> void:
+	can_handle_action_input = false
+
 	assert(active_battle == null)
 
 	active_battle = battle_scene.instantiate()
@@ -70,3 +82,5 @@ func _start_battle() -> void:
 	# as currently it attaches the player to its root to correctly display it
 	get_tree().root.add_child(player)
 	active_battle.queue_free()
+
+	can_handle_action_input = true
